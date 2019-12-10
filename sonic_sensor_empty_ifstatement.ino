3319343c6d1978f16e73c44e100b5b0e33757d27
nreimer204 <57808634+nreimer204@users.noreply.github.com>
2019-12-10 11:43:34 -0800

    Add files via upload

---
+++ sonic_sensor_empty_ifstatement.ino
@@ -0,0 +1,41 @@
+/*  
+    Arduino with PIR motion sensor
+    For complete project details, visit: http://RandomNerdTutorials.com/pirsensor
+    Modified by Rui Santos based on PIR sensor by Limor Fried
+*/
+
+
+ 
+int sensor = 2;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;                 // variable to store the sensor status (value)
+
+
+
+
+void setup() {
+  pinMode(sensor, INPUT);    // initialize sensor as an input
+  Serial.begin(9600);        // initialize serial
+
+  
+}
+
+void loop(){
+  val = digitalRead(sensor);   // read sensor value
+ 
+  if (val == HIGH) {           // check if the sensor is HIGH
+
+//Put the code for what you want to happen when the sensor is triggered here
+    
+  }
+  
+  else {
+
+//Put the code for what should happen when the sensor is not being triggered here
+
+    }
+
+}
+
+
+//Put the small bits of repeatable instructions (functions) here:
